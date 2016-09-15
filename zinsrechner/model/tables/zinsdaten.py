ef7020a5a4bfed547ec87ad0d21cd79ee44409c4
Michael <michael.kamfor@tu-dortmund.de>
2016-09-15 17:07:34 +0200

    Add files via upload

--- zinsrechner/model/tables/zinsdaten.py
+++ zinsrechner/model/tables/zinsdaten.py
@@ -7,39 +7,46 @@ from enum import Enum
 from util.util import util as ut
 
 class ZDATA():
-    
+
     NAME = 'zdata'
 
     HIDE_COLUMNS = ['TFID']
-    
+
     COLUMN_ORDER = [
         'FORMELID ASC'
     ]
-        
+
     # important data with their column names
     class COLUMN(Enum):
-        DATUM='DATUM'
-        RESTSCHULD='RESTSCHULD'
-        ZKOSTEN='ZKOSTEN'
-        
-        
+        DATUM='Datum'
+        RESTSCHULD='Restschuld'
+        ZKOSTEN='Zinskosten'
+
+
     COLUMNS = [
-        'DATUM', 'RESTSCHULD', 'ZKOSTEN'
+        'Datum', 'Restschuld', 'Zinskosten'
     ]
-    
-        
-    COLUMNS_AND_TYPES = "'DATUM' DATE, 'RESTSCHULD' REAL, 'ZKOSTEN' REAL"
+
+    DATECOLUMNS = [
+        'Datum'
+    ]
+
+    FLOATCOLUMNS = [
+        'Restschuld', 'Zinskosten'
+    ]
+
+    COLUMNS_AND_TYPES = "'Datum' DATE, 'Restschuld' REAL, 'Zinskosten' REAL"
 
 
-    
+
     def __init__(self, dictionary):
-        
+
         self.finSumme   = 0
         self.rate       = 0
         self.effJZ      = 0
         self.sollZB     = 0
         self.beginn     = 0
-        
+
         for name, val in dictionary.items():
             if name == "FinSumme" and len(str(val)) > 0:
                 setattr(self, 'finSumme', ut.str_to_int(val))
@@ -51,7 +58,7 @@ class ZDATA():
                 setattr(self, 'sollZB',   ut.str_to_int(val))
             if name == "Beginn"   and len(str(val)) > 0:
                 setattr(self, 'beginn',   ut.dat_konv(val))
-            
+
     def print_para(self):
         out = [["param" , "wert"],\
                ["------", "----------"],\
@@ -62,7 +69,7 @@ class ZDATA():
                ["beginn", self.beginn],\
                ["----------------------",""]]
         ut.view_2d_list(out)
-    
+
     def status_ok(self):
         try:
             if self.finSumme  > 0 \
@@ -74,21 +81,21 @@ class ZDATA():
                 return False
         except:
             return False
-    
+
     def forward_projection(self):
-        
+
         out = []
         rest   = self.finSumme
-        kosten = 0
+        kosten = 0.0
         maxDauer = 12*50 + 1
-        
+
         if self.finSumme * (self.effJZ/12) / 100 > self.rate:
             print("Rate ist zu klein")
             return
-        
+
         for step in range(0, maxDauer):
             tmpDate  = ut.add_month(self.beginn, step)
-            out     += [(tmpDate, "%.2f" % rest, "%.2f" % kosten)]
+            out     += [(tmpDate, rest, kosten)]
 #############################################
 #           Hier ist des Pudels Kern
 #############################################
@@ -100,15 +107,15 @@ class ZDATA():
 #############################################
 #############################################
         return out
-    
+
     def eval_cost(self, val):
         dauerSZB   = 12 * self.sollZB
         dauerTotal = len(val)
-        
+
         restSchuld  = 0
         kostenSZB   = 0
         kostenTotal = val[-1][1]
-        
+
         if dauerTotal >= dauerSZB:
             kostenSZB  = val[dauerSZB][1]
             restSchuld = val[dauerSZB][0]
